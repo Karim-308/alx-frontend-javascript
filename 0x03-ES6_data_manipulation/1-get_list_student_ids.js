3e9529af401291576650c8b78f12aec7bd710c52
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-08-14 01:20:11 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,6 @@
-export default function getListStudentIds (){
-
+export default function getListStudentIds (students){
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map(student => student.id););
 }
