c1a0a1b479e2881541309323bbb7c91ce3ab6501
Karim-308 <2001118@eng.asu.edu.eg>
2024-08-15 04:24:38 +0300

    main0

---
+++ 0x04-TypeScript/task_0/main.ts
@@ -0,0 +1,32 @@
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+}
+
+const student1: Student = {
+	firstName: "Karim";
+        lastName: "Ibrahim";
+        age: 30;
+        location: "Cairo";  
+	}
+
+
+const student1: Student = {
+        firstName: "Karim2";
+	lastName: "Ibrahim2";
+	age: 30;
+	location: "Cairo";
+}
+
+
+const studentsList = [student1 , student2]
+
+
+
+foreach s in studentsList{
+
+	console.log(s.firstName+ " " s.Location)
+
+}
