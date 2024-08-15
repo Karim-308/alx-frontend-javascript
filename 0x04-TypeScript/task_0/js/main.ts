431aa4b081bf5f04028ffe9baa33a623abc47a7b
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-08-15 04:35:20 +0300

    Update main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,29 +1,43 @@
-
 interface Student {
-	firstName: string;
-	lastName: string;
-	age: number;
-	location: string;
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
 }
 
 const student1: Student = {
-	firstName: "Karim",
+    firstName: "Karim",
     lastName: "Ibrahim",
     age: 30,
     location: "Cairo"
-	}
-
+};
 
 const student2: Student = {
-        firstName: "Karim2",
-	lastName: "Ibrahim2",
-	age: 30,
-	location: "Cairo"
-}
-
+    firstName: "Karim2",
+    lastName: "Ibrahim2",
+    age: 30,
+    location: "Cairo"
+};
 
 const studentsList = [student1, student2];
 
 studentsList.forEach(s => {
     console.log(s.firstName + " " + s.location);
 });
+
+const table = document.createElement("table");
+studentsList.forEach(s => {
+    const row = document.createElement("tr");
+
+    const nameCell = document.createElement("td");
+    const locationCell = document.createElement("td");
+
+    nameCell.textContent = s.firstName;
+    locationCell.textContent = s.location;
+
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    table.appendChild(row);
+});
+
+document.body.appendChild(table);
