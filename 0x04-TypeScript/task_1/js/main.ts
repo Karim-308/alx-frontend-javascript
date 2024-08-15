627f9d5c9853bb94236e7336d87c37b00e6299ee
Karim-308 <2001118@eng.asu.edu.eg>
2024-08-15 05:32:07 +0300

    main4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,4 +17,32 @@ function printTeacher(firstName: string ,lastName: string): string {
 	return firstName[0] + "." + lastName;
 }
 
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor({ firstName, lastName }: StudentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
 
